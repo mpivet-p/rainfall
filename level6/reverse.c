3378088c9eb1fc4e3e80abc19af65e62252d3d52
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2021-08-19 20:58:10 +0200

    Level 6 done. Need to do explainations

--- level6/reverse.c
+++ level6/reverse.c
@@ -1,1 +1,28 @@
-int		main(void)
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void	n()
+{
+	system("/bin/cat /home/user/level7/.pass");
+}
+
+void	m()
+{
+	puts("Nope");
+}
+
+int		main(int argc, char **argv)
+{
+	char	*ptr;
+	char	*a;
+	void	*func;
+
+	a = malloc(64);
+	func = malloc(4);
+	//set func to m()
+	ptr = argv[1];
+	ptr += 4;
+	strcpy(a, ptr);
+	//call func();
+}
