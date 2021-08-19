cb00f263f52caf95954f585d132d25ce29fe91e7
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2021-08-19 16:05:01 +0200

    Level5 done

---
+++ level5/reverse.c
@@ -0,0 +1,21 @@
+int m;
+
+void o(void)
+{
+	system("/bin/sh");
+	exit(1);
+}
+
+void n(void)
+{
+	char buf[512];
+
+	fgets(buf, 512, stdin);
+	printf(buf);
+	exit(1);
+}
+
+int main(void)
+{
+	n();
+}
