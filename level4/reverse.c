49995274a74772d422c5a0c8c8319b287672ff09
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2021-08-19 10:51:53 +0200

    Level 4 done. This one was pretty easy

---
+++ level4/reverse.c
@@ -0,0 +1,23 @@
+int	m;
+
+void p(char *ptr)
+{
+	printf(ptr);
+}
+
+void n()
+{
+	char buf[512];
+
+	fgets(buf, 512, stdin);
+	p(buf);
+	if (m == 16930116)
+	{
+		system("/bin/cat /home/user/level5/.pass");
+	}
+}
+
+int	main(void)
+{
+	n();
+}
