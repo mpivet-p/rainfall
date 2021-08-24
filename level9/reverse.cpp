bf2dd79f8d3cec3b0c4f030d04dcfc6c22784751
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2021-08-24 19:13:51 +0200

    level9 reverse almost done. Need to exploit now

---
+++ level9/reverse.cpp
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <string.h>
+//#include <stdlib.h>
+
+class N {
+
+private:
+	char	_annotation[100];
+	int		_number;
+
+public:
+	N(int n) : _number(n) {} ;
+
+	void setAnnotation(char *str) {
+		memcpy(_annotation, str, strlen(str));
+	}
+
+	virtual int operator+(const N& other) {
+		return (this->_number + other._number);
+	}
+
+	virtual int operator-(const N& other) {
+		return (this->_number - other._number);
+	}
+
+};
+
+int		main(int argc, char *argv[])
+{
+	N *a;
+	N *b;
+
+	if (argc <= 1)
+	{
+		_exit(1);
+	}
+
+	a = new N(5); //_Znwj <=> new    108 (0x6c) => size of instance
+	b = new N(6);
+
+	a->setAnnotation(argv[1]);
+	return (*a + *b); //_ZN1NplERS_(esp+0x14, esp+0x10)
+}
